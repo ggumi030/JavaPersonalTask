2101620a7070a076783b44c0629c302aac595994
Robbie <robbie331@>
2024-04-29 20:15:33 +0900

    level2-6 생성자 구현 수정 + level2-7 원 넓이 메서드 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,66 +6,107 @@ public class App {
 
     public static void main(String[] args) throws ArithmeticException{
         Scanner sc = new Scanner(System.in);
-
         Calculator calc = new Calculator();
 
         while(true){
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
+            //연산 선택
+            System.out.println("원하는 연산을 선택해주세요. (arithmetic, circle)");
+            String selection = sc.nextLine();
 
-            double result = 0;
+            //사칙연산
+            if(selection.equals("arithmetic")){
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int firstNum = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int secondNum = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+
+                double arithmeticResult = 0;
 
                 switch (operator){
                     case '+':
-                        result = calc.calculate(firstNum,secondNum,'+');
-                        calc.Setter(result);
+                        arithmeticResult = calc.calculate(firstNum,secondNum,'+');
+                        calc.arithmeticSetter(arithmeticResult);
                         break;
                     case '-':
-                        result = calc.calculate(firstNum,secondNum,'-');
-                        calc.Setter(result);
+                        arithmeticResult = calc.calculate(firstNum,secondNum,'-');
+                        calc.arithmeticSetter(arithmeticResult);
                         break;
                     case '*':
-                        result = calc.calculate(firstNum,secondNum,'*');
-                        calc.Setter(result);
+                        arithmeticResult = calc.calculate(firstNum,secondNum,'*');
+                        calc.arithmeticSetter(arithmeticResult);
                         break;
                     case '/':
-                        result = calc.calculate(firstNum,secondNum,'/');
-                        calc.Setter(result);
+                        arithmeticResult = calc.calculate(firstNum,secondNum,'/');
+                        calc.arithmeticSetter(arithmeticResult);
                         break;
                     default:
-                        result = calc.calculate(firstNum,secondNum,operator);
+                        arithmeticResult = calc.calculate(firstNum,secondNum,operator);
                 }
 
+                System.out.println("결과: " + arithmeticResult);
 
-            System.out.println("결과: " + result);
+                //값 제거
+                sc.nextLine(); //입력된 \n값 제거
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.nextLine();
 
+                if(remove.equals("remove")){
+                    calc.arithmeticRemoveResult();
+                }
 
-            //값 제거
-            sc.nextLine(); //입력된 \n값 제거
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.nextLine();
+                //값 조회
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.nextLine();
+                if(inquiry.equals("inquiry")){
+                    calc.arithmeticInquiryResults();
+                }
 
-            if(remove.equals("remove")){
-                calc.removeResult();
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String re = sc.nextLine();
+                if(re.equals("exit")){
+                    break;
+                }
             }
 
-            //값 조회
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = sc.nextLine();
-            if(inquiry.equals("inquiry")){
-                calc.inquiryResults();
+            //원 넓이 구하기
+            if(selection.equals("circle")){
+                System.out.print("반지름의 길이를 입력하세요: ");
+                double circleRadius = sc.nextDouble();
+                double circleResult;
+
+                circleResult =calc.calculateCircleArea(circleRadius);
+
+                //넓이 저장
+                calc.circleSetter(circleResult);
+
+                //값 제거
+                sc.nextLine(); //입력된 \n값 제거
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.nextLine();
+
+                if(remove.equals("remove")){
+                    calc.circleRemoveResult();
+                }
+
+                //값 조회
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.nextLine();
+                if(inquiry.equals("inquiry")){
+                    calc.circleInquiryResults();
+                }
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String re = sc.nextLine();
+                if(re.equals("exit")){
+                    break;
+                }
             }
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String re = sc.nextLine();
-            if(re.equals("exit")){
-                break;
-            }
+
         }
+
 
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,27 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class Calculator {
+    //사칙 연산 컬렉션
     private ArrayList<Double> arrList;
+    //원의 넓이 컬렉션
+    private ArrayList<Double> circleList = new ArrayList<>();
+    //파이 값
+    public static final double PI = Math.PI;
+    //파이 값은 변하지 않는 상수이기 때문에 final로 선언
+    //객체 생성할 때마다 새로 메모리를 할당해주면 같은 값인데 쓸데없이 메모리가 중복되어 사용되니, 상수는 한번 선언해둔 메모리를 참조해서 사용할 수 있도록 static으로 선언
 
+
+    //생성자
     public Calculator(){
         arrList = new ArrayList<>();
+        circleList = new ArrayList<>();
     }
 
+
+    //사칙 연산 계산 메서드
     public double calculate(int firstNum, int secondNum, char opterator) throws ArithmeticException{
         double result = 0.0;
 
@@ -33,19 +46,19 @@ public class Calculator {
         return result;
     }
 
-    public void Setter(double result){
+    public void arithmeticSetter(double result){
         arrList.add(result);
     }
 
-    public ArrayList<Double> getArrList(){
+    public ArrayList<Double> arithmeticGetter(){
         return arrList;
     }
 
-    public void removeResult(){
+    public void arithmeticRemoveResult(){
         arrList.removeFirst();
     }
 
-    public void inquiryResults(){
+    public void arithmeticInquiryResults(){
         System.out.print("[ ");
         for(Double num : arrList){
             System.out.print(num + " ");
@@ -54,5 +67,29 @@ public class Calculator {
     }
 
 
+    //원의 넓이 계산 메서드
+    public double calculateCircleArea(double radius){
+        double result = radius * radius * PI;
+        return result;
+    }
 
+    public ArrayList<Double> circleGetter(){
+        return circleList;
+    }
+
+    public void circleSetter(double result){
+        circleList.add(result);
+    }
+
+    public void circleRemoveResult(){
+        circleList.removeFirst();
+    }
+
+    public void circleInquiryResults(){
+        System.out.print("[ ");
+        for(Double num : circleList){
+            System.out.print(num + " ");
+        }
+        System.out.println("]");
+    }
 }
